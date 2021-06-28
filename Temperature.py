f933bd266b457c9186d1ff5755a42c17cbb41686
Yoru <77332897+Yoru81@users.noreply.github.com>
2021-06-28 07:36:28 +0530

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,18 @@
+while True:
+    temperature = int(input("What is the Temperature? "))
+
+    if temperature > 30:
+        print("It's a hot day")
+        print("Drink Plenty of water")
+    elif temperature > 20:
+        print("It's a nice day")
+    elif temperature > 10:
+        print("It's a bit cold")
+    else:
+        print("It's cold")
+
+
+
+        
+
+
