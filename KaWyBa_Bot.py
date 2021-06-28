78cc680eea1bfeb10ce314cdb646aa08ef772fda
Yoru <77332897+Yoru81@users.noreply.github.com>
2021-06-28 09:22:22 +0530

    Update KaWyBa_Bot.py

--- KaWyBa_Bot.py
+++ KaWyBa_Bot.py
@@ -177,9 +177,20 @@ while True:
 
 
     elif enter_command.upper() == "KILL":
-        target = input("Who do you want to kill?: ")
+        import random
 
-        print(target + " Died by fallling of a clif! ")
+        target = input("Who do you want to kill?(Enter name): ")
+        typeofdeath = input("How do you want " + target + " to die?(Painful , Peaceful , Natural): ")
+
+        if typeofdeath.upper() == "PAINFUL":
+            painfuldeath = [" Died by falling  into magma! " , " Died by being eaten by the most monsterous DEMON! "]
+            print(target + random.choice(painfuldeath))
+        elif typeofdeath.upper() == "PEACEFUL":
+            peacefuldeath = [" Was Chilling in the cool breeze out in his patio, and just passed away as the wind went by." , " Ate a medicine which gave him/her death with no pain. "]
+            print(target + random.choice(peacefuldeath))
+        elif typeofdeath.upper() == "NATURAL":
+            naturaldeath = " Just turned 100!(Happy Birthday!)The next second, He passed away due to his age. "
+            print(target + naturaldeath)
 
 
     elif enter_command.upper() == "OE":
